a43ece013423894e5a1e61eddad8eaa08d156409
Martin <jchristofmartin@gmail.com>
2025-05-11 20:59:41 -0600

    Clean up tile maps. Add a three-layer tile map.

--- examples/rogue/assets/sewers3.tsx
+++ examples/rogue/assets/sewers3.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="sewers_16" tilewidth="16" tileheight="16" tilecount="160" columns="20">
  <image source="../graphics/sewers_16.bmp" trans="ebf4f3" width="320" height="128"/>
+ <tile id="1">
+  <properties>
+   <property name="floor" value=""/>
+  </properties>
+ </tile>
 </tileset>
